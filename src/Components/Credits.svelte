fd31c834b594a51eb61354f9c4d3fd8616a44c1a
Jason <jmaalegado@gmail.com>
2021-04-09 20:02:54 +1000

    added new Credits component

---
+++ src/Components/Credits.svelte
@@ -0,0 +1,14 @@
+<style>
+    .credits {
+        margin-top: 24px;
+
+        font-size: 0.8rem;
+        text-align: center;
+    }
+</style>
+
+<div class="credits">
+    <p>Test icons are from flaticon.com.</p>
+    <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>		
+</div>
